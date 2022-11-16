a83ade5cdfafe03352baa044743d065818d45b7e
Felipe Gavilan <elmejor_887@hotmail.com>
2022-11-16 11:57:26 -0400

    Add project files.

---
+++ BlazorPreventLeaving/Client/Program.cs
@@ -0,0 +1,12 @@
+using BlazorPreventLeaving.Client;
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSweetAlert2();
+await builder.Build().RunAsync();
